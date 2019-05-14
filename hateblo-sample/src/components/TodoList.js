d8381cde72e558d27a98a6d9dfe1fce033f3e47a
Junya Kobayashi <atsubeee@doi.ics.keio.ac.jp>
2019-05-14 23:39:36 +0900

    えらーなんでだろ

--- hateblo-sample/src/components/TodoList.js
+++ hateblo-sample/src/components/TodoList.js
@@ -1,34 +1,33 @@
 import React from 'react'
+import { isLoaded, isEmpty } from 'react-redux-firebase'
 import PropTypes from 'prop-types'
 import Todo from './Todo'
 
-const TodoList = ({
-  todos,
-  toggleTodo
-}) => ( <ul > {
-    todos.map(todo => ( <
-      Todo key = {
-        todo.id
-      } {
-        ...todo
-      }
-      onClick = {
-        () => toggleTodo(todo.id)
-      }
-      />
-    ))
-  } </ul>
-)
+const TodoList = ({ todos, toggleTodo }) => {
+  if (!isLoaded(todos)) {
+    return <div>タスク一覧読み込み中...</div>
+  }
+  if (isEmpty(todos)) {
+    return <div>タスクがありません</div>
+  }
+
+  return (
+    <ul>
+      {Object.keys(todos).map((key) => (
+        <Todo key={key} {...todos[key]} onClick={() => toggleTodo(key)} />
+      ))}
+    </ul>
+  )
+}
 
 TodoList.propTypes = {
-  todos: PropTypes.arrayOf(
+  todos: PropTypes.objectOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
       completed: PropTypes.bool.isRequired,
       text: PropTypes.string.isRequired
-    }).isRequired
-  ).isRequired,
-  toggleTodo: PropTypes.func.isRequired
+    })
+  ),
+  onTodoClick: PropTypes.func.isRequired
 }
 
 export default TodoList
--- hateblo-sample/src/containers/VisibleTodoList.js
+++ hateblo-sample/src/containers/VisibleTodoList.js
@@ -1,7 +1,9 @@
 import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
-import { VisibilityFilters} from '../actions'
+import { VisibilityFilters } from '../actions'
+import { compose } from 'redux'
+import { firebaseConnect } from 'react-redux-firebase'
 
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
@@ -17,12 +19,23 @@ const getVisibleTodos = (todos, filter) => {
 }
 
 const mapStateToProps = state => ({
-  todos: getVisibleTodos(state.todos, state.visibilityFilter)
+  todos: getVisibleTodos(state.firebase.data.todos, state.visibilityFilter)
 })
 
 const mapDispatchToProps = dispatch => ({
   toggleTodo: id => dispatch(toggleTodo(id))
 })
+
+const firebaseQueries = ['todos'];
+
+const VisibleTodoList = compose(
+  firebaseConnect(firebaseQueries),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(TodoList)
+
 
 export default connect(
   mapStateToProps,
