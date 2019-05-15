dbdc31217d450348d2a813206a0010e80721ca18
Junya Kobayashi <atsubeee@doi.ics.keio.ac.jp>
2019-05-16 07:16:39 +0900

    一旦進むことにした

--- hateblo-sample/src/containers/VisibleTodoList.js
+++ hateblo-sample/src/containers/VisibleTodoList.js
@@ -1,22 +1,51 @@
+import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
 import { VisibilityFilters } from '../actions'
-import { compose } from 'redux'
+
 import { firebaseConnect } from 'react-redux-firebase'
 
 const getVisibleTodos = (todos, filter) => {
+  if (!todos) return todos
   switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
+    case 'SHOW_ALL':
       return todos
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(t => t.completed)
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(t => !t.completed)
+    case 'SHOW_COMPLETED':
+      return Object.keys(todos)
+        .filter(key => todos[key].completed)
+        .reduce((filtered, key) => {
+          filtered[key] = todos[key];
+          return filtered;
+        },
+          {}
+        )
+    case 'SHOW_ACTIVE':
+      return Object.keys(todos)
+        .filter(key => !todos[key].completed)
+        .reduce((filtered, key) => {
+          filtered[key] = todos[key]
+          return filtered;
+        },
+          {}
+        )
     default:
       throw new Error('Unknown filter: ' + filter)
   }
 }
+
+// const getVisibleTodos = (todos, filter) => {
+//   switch (filter) {
+//     case VisibilityFilters.SHOW_ALL:
+//       return todos
+//     case VisibilityFilters.SHOW_COMPLETED:
+//       return todos.filter(t => t.completed)
+//     case VisibilityFilters.SHOW_ACTIVE:
+//       return todos.filter(t => !t.completed)
+//     default:
+//       throw new Error('Unknown filter: ' + filter)
+//   }
+// }
 
 const mapStateToProps = state => ({
   todos: getVisibleTodos(state.firebase.data.todos, state.visibilityFilter)
